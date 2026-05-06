e5ba2aa2f4a5ceef54dbc4d34ebdbaa241e91dbe
daisy <moonp7472@gmail.com>
2026-05-05 20:03:47 -0400

    Add BoneEnums.hpp with bone enumeration definitions

---
+++ BoneEnums.hpp
@@ -0,0 +1,24 @@
+namespace enums {
+  enum e_bones : std::uint8_t
+	{
+		root        = 0,
+		pelvis      = 8,
+		spine_01    = 11,
+		spine_02    = 10,
+		spine_03    = 9,
+		neck_01     = 62,
+		head        = 63,
+		upperarm_l  = 13,
+		lowerarm_l  = 14,
+		hand_l      = 15,
+		upperarm_r  = 38,
+		lowerarm_r  = 39,
+		hand_r      = 40,
+		upperleg_l  = 64,
+		lowerleg_l  = 65,
+		foot_l      = 67,
+		upperleg_r  = 70,
+		lowerleg_r  = 71,
+		foot_r      = 73,
+	};
+}
